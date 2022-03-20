bdf04dac21466523065d8acb15b5424f2c7a63ea
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-20 17:11:23 +0100

    login query

--- sql/queries.sql
+++ sql/queries.sql
@@ -5,3 +5,12 @@ INSERT INTO
   `users` (`id`, `email`, `password`)
 VALUES
   ('admin', 'adming@admin.es', 'Admin+12')
+
+/* login */
+SELECT
+  id
+FROM
+  users
+WHERE
+  users.id = 'admin'
+  AND users.password = 'Admin+12';

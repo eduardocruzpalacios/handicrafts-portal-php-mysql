ccdcbaec430ac624ac08184463d5831fdc7c4239
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-20 17:54:06 +0100

    refactor login as view

--- index.php
+++ index.php
@@ -37,26 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <body>
   <?php include './views/header.html'?>
-  <section>
-    <h2>Login</h2>
-    <form action="" method="post">
-      <fieldset>
-        <legend>User data</legend>
-        <label for="user">Username:</label>
-        <input type="text" placeholder="Sancho Panza" id="user" name="user" required autofocus>
-        <label for="password">Password:</label>
-        <input type="password" placeholder="********" id="password" name="password" required maxlength="20" minlength="8">
-        <input type="submit" name="" id="" value="Log in">
-        <?php if (isset($error)) : ?>
-          <span>
-            <?php
-            echo $error;
-            ?>
-          </span>
-        <?php endif; ?>
-      </fieldset>
-    </form>
-  </section>
+  <?php include './views/login.php'?>
 </body>
 
 </html>

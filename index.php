8dc30a52bb9845af9061ed07ce33dbe0d66f7890
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-20 18:00:16 +0100

    add Bootstrap styles

--- index.php
+++ index.php
@@ -33,11 +33,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta name="author" content="Eduardo de la Cruz Palacios">
   <meta name="copyright" content="Eduardo de la Cruz Palacios" />
   <title>Handicraft | Login</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
-  <?php include './views/header.html'?>
-  <?php include './views/login.php'?>
+  <?php include './views/header.html' ?>
+  <?php include './views/login.php' ?>
 </body>
 
 </html>

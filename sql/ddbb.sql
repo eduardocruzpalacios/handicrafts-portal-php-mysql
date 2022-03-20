feed2edd867f90199fc8bb2a216345bf12f034de
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-20 17:02:41 +0100

    create users table for ddbb

--- sql/ddbb.sql
+++ sql/ddbb.sql
@@ -3,3 +3,11 @@
 CREATE DATABASE handicraft
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+
+/* TABLE USERS */
+CREATE TABLE `handicraft`.`users` (
+  `id` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;

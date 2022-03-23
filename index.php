ad137b320a9c272f64ecdd1b4e9e7d0d480201a6
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-23 22:45:35 +0100

    separate index and login form into 2 pages

--- index.php
+++ index.php
@@ -1,27 +1,13 @@
 <?php
+
 require 'service-ddbb.php';
 require 'model/handicraft.php';
 require 'service-handicraft-read-all.php';
 
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-  $id = $_POST['user'];
-  $password = $_POST['password'];
-
-  $result = login($id, $password);
-
-  if ($result) {
-    setcookie('user', $id, time() + 3600);
-    header('Location: admin.php');
-    exit();
-  } else {
-    $error = 'Wrong user or password';
-  }
-}
-
 loadHandicraftOnSession();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +18,7 @@ loadHandicraftOnSession();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="author" content="Eduardo de la Cruz Palacios">
   <meta name="copyright" content="Eduardo de la Cruz Palacios" />
-  <title>Handicraft | Login</title>
+  <title>Handicraft</title>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -40,9 +26,7 @@ loadHandicraftOnSession();
 <body>
   <?php include './views/header.php' ?>
   <?php include './views/nav.php' ?>
-  <?php include './views/login_form.php' ?>
   <section>
-    <!-- PRINT HANDICRAFT -->
     <?php for ($x = 0; $x < count($_SESSION['handicraft']); $x++) : ?>
       <article>
         <p><?php echo $_SESSION['handicraft'][$x]->get_id(); ?></p>

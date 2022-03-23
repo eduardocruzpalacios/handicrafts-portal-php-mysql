8d53a56d66b5ccae81cae02fe2d292cf56d429c7
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-23 22:46:23 +0100

    islogged cookie logic when logout

--- service-logout.php
+++ service-logout.php
@@ -3,6 +3,8 @@ session_start();
 session_destroy();
 setcookie('user', $user, time() - 3600);
 unset($_COOKIE['user']);
+setcookie('islogged', 'islogged', time() - 3600);
+unset($_COOKIE['islogged']);
 header('Location: ./');
 exit();
 ?>

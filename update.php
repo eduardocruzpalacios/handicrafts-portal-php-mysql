bdf7f4da155ba64a61593fe0763bf1731c7eeef2
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-26 16:12:59 +0100

    create update handicraft page

---
+++ update.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+  header('Location: admin.php');
+  exit();
+} 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Eduardo de la Cruz Palacios">
+  <meta name="copyright" content="Eduardo de la Cruz Palacios" />
+  <title>Handicraft | Admin - update</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <?php include './views/header.php' ?>
+  <?php include './views/nav.php' ?>
+
+  <h2>Update this handicraft</h2>
+
+</body>
+
+</html>

4fe2778f87c308ee49234a98c4b01a0b8e2096f0
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-02 20:23:33 +0200

    login

--- views/login_form.php
+++ views/login_form.php
@@ -1,13 +1,17 @@
-<section>
+<section class="container">
   <h2>Login</h2>
   <form action="" method="post">
     <fieldset>
       <legend>User data</legend>
-      <label for="user">User id:</label>
-      <input type="text" placeholder="Sancho Panza" id="user" name="user" required autofocus>
-      <label for="password">Password:</label>
-      <input type="password" placeholder="********" id="password" name="password" required maxlength="20" minlength="8">
-      <input type="submit" name="" id="" value="Log in">
+      <div class="form-group mb-3">
+        <label for="user">User id:</label>
+        <input type="text" placeholder="Sancho Panza" id="user" name="user" required autofocus class="form-control">
+      </div>
+      <div class="form-group mb-3">
+        <label for="password">Password:</label>
+        <input type="password" placeholder="********" id="password" name="password" required maxlength="20" minlength="8" class="form-control">
+      </div>
+      <input type="submit" name="" id="" value="Log in" class="btn btn-primary">
       <?php if (isset($error)) : ?>
         <span>
           <?php

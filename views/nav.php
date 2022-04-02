f4fbbc4b732044487fdc1a82058af4e9bc6b3cbc
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-02 19:01:17 +0200

    delete unnecessary cookie

--- views/nav.php
+++ views/nav.php
@@ -1,11 +1,11 @@
 <nav>
   <ul>
     <li><a href="./">Home</a></li>
-    <?php if (!isset($_COOKIE['islogged'])) : ?>
+    <?php if (!isset($_COOKIE['user'])) : ?>
       <li><a href="login.php">Log in</a></li>
       <li><a href="signup.php">Sign up</a></li>
     <?php endif; ?>
-    <?php if (isset($_COOKIE['islogged'])) : ?>
+    <?php if (isset($_COOKIE['user'])) : ?>
       <li><a href="admin.php">Admin</a></li>
       <li><a href="service-logout.php">Logout</a></li>
     <?php endif; ?>

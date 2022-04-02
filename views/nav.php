8e200c628a012c5a01cda417f4011c80c8cb4a36
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-02 20:13:32 +0200

    nav

--- views/nav.php
+++ views/nav.php
@@ -1,16 +1,23 @@
-<nav>
-  <ul>
-    <li><a href="./">Home</a></li>
-    <?php if (!isset($_COOKIE['user'])) : ?>
-      <li><a href="login.php">Log in</a></li>
-      <li><a href="signup.php">Sign up</a></li>
-    <?php endif; ?>
-    <?php if (isset($_COOKIE['user'])) : ?>
-      <li><a href="admin.php">Admin</a></li>
-      <li><a href="service-logout.php">Logout</a></li>
-    <?php endif; ?>
-  </ul>
-  <?php if (isset($_COOKIE['user'])) : ?>
-    <span>Welcome <?php echo $_COOKIE['user'] ?></span>
-  <?php endif; ?>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top p-4">
+    <ul class="navbar-nav">
+      <li class=" nav-item">
+        <a href="./" class="nav-link">Home</a>
+      </li>
+      <?php if (!isset($_COOKIE['user'])) : ?>
+        <li class="nav-item">
+          <a href="login.php" class="nav-link">Log in</a>
+        </li>
+        <li class="nav-item">
+          <a href="signup.php" class="nav-link">Sign up</a>
+        </li>
+      <?php endif; ?>
+      <?php if (isset($_COOKIE['user'])) : ?>
+        <li class="nav-item">
+          <a href="admin.php" class="nav-link">Admin</a>
+        </li>
+        <li class="nav-item">
+          <a href="service-logout.php" class="nav-link">Logout</a>
+        </li>
+      <?php endif; ?>
+    </ul>
 </nav>

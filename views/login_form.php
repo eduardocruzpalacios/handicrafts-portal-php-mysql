6c5c2b9cdae8a9bd80175b42c06791c7e120f655
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-02 20:53:01 +0200

    add form-label classes

--- views/login_form.php
+++ views/login_form.php
@@ -2,15 +2,15 @@
   <form action="" method="post">
     <fieldset>
       <legend>User data</legend>
-      <div class="form-group mb-3">
-        <label for="user">User id:</label>
+      <div class="form-group">
+        <label for="user" class="form-label">User id:</label>
         <input type="text" placeholder="Sancho Panza" id="user" name="user" required autofocus class="form-control">
       </div>
-      <div class="form-group mb-3">
-        <label for="password">Password:</label>
+      <div class="form-group">
+        <label for="password" class="form-label">Password:</label>
         <input type="password" placeholder="********" id="password" name="password" required maxlength="20" minlength="8" class="form-control">
       </div>
-      <input type="submit" name="" id="" value="Log in" class="btn btn-primary">
+      <input type="submit" name="" id="" value="Log in" class="btn btn-primary mt-3">
       <?php if (isset($error)) : ?>
         <span>
           <?php
--- views/signup_form.php
+++ views/signup_form.php
@@ -2,30 +2,30 @@
   <form action="" method="post">
     <fieldset>
       <legend>New user data</legend>
-      <div class="form-group mb-3">
-        <label for="id">User ID:</label>
+      <div class="form-group">
+        <label for="id" class="form-label">User ID:</label>
         <input type="text" placeholder="sancho0panza" id="id" name="id" required autofocus maxlength="20" class="form-control">
       </div>
-      <div class="form-group mb-3">
-        <label for="name">Name:</label>
+      <div class="form-group">
+        <label for="name" class="form-label">Name:</label>
         <input type="text" placeholder="Sancho Panza" id="name" name="name" required maxlength="30" class="form-control">
       </div>
-      <div class="form-group mb-3">
-        <label for="email">Email:</label>
+      <div class="form-group">
+        <label for="email" class="form-label">Email:</label>
         <input type="email" placeholder="sancho.panza@lamancha.es" id="email" name="email" required maxlength="50" class="form-control">
       </div>
-      <div class="form-group mb-3">
-        <label for="password">Password:</label>
+      <div class="form-group">
+        <label for="password" class="form-label">Password:</label>
         <input type="password" placeholder="********" id="password" name="password" required maxlength="20" minlength="8" class="form-control">
       </div>
-        <input type="submit" name="" id="" value="Signup" class="btn btn-primary">
-        <?php if (isset($error)) : ?>
-          <span>
-            <?php
-            echo $error;
-            ?>
-          </span>
-        <?php endif; ?>
+      <input type="submit" name="" id="" value="Signup" class="btn btn-primary mt-3">
+      <?php if (isset($error)) : ?>
+        <span>
+          <?php
+          echo $error;
+          ?>
+        </span>
+      <?php endif; ?>
     </fieldset>
   </form>
 </section>
